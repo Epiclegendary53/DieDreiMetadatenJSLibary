8416b66f6d0b53dd6e9bd84231daebb1ba4fd4fe
Jan Wagner <ich-mag-bananen@gmx.net>
2025-10-18 22:00:31 +0200

    Einige klassen für besseres Handling erstellt. Weitere getter hinzugefügt

--- classes.ts
+++ classes.ts
@@ -5,22 +5,22 @@ class hörspiel {
     #hörspielskriptautor: hörspielskriptautor;
     #gesamtbeschreibung: string;
     #beschreibung: string;
-    #veröffentlichungdatum: Date;
-    #gesamtdauer: number;
-    #kapitel: kapitel[];
-    #sprecherrollen: sprecherrolle[];
-    #links: links[];
-    #ids: ids[];
-    #medien: medien[];
+    #veröffentlichungsdatum: veröffentlichungsdatum;
+    #gesamtdauer: gesamtdauer;
+    #kapitel: Array<kapitel>;
+    #sprecherrollen: Array<sprecherrolle>;
+    #links: links;
+    #ids: ids;
+    #medien: Array<medium>;
 
-    constructor(nummer: number, titel: string, autor: autor, hörspielskriptautor: hörspielskriptautor, gesamtbeschreibung: string, beschreibung: string, veröffentlichungdatum: Date, gesamtdauer: number, kapitel: kapitel[], sprecherrollen: sprecherrolle[], links: links[], ids: ids[], medien: medien[]) {
+    constructor(nummer: number, titel: string, autor: autor, hörspielskriptautor: hörspielskriptautor, gesamtbeschreibung: string, beschreibung: string, veröffentlichungsdatum: veröffentlichungsdatum, gesamtdauer: gesamtdauer, kapitel: kapitel[], sprecherrollen: sprecherrolle[], links: links, ids: ids, medien: medium[]) {
         this.#nummer = nummer;
         this.#titel = titel;
         this.#autor = autor;
         this.#hörspielskriptautor = hörspielskriptautor;
         this.#gesamtbeschreibung = gesamtbeschreibung;
         this.#beschreibung = beschreibung;
-        this.#veröffentlichungdatum = veröffentlichungdatum;
+        this.#veröffentlichungsdatum = veröffentlichungsdatum;
         this.#gesamtdauer = gesamtdauer;
         this.#kapitel = kapitel;
         this.#sprecherrollen = sprecherrollen;
@@ -29,6 +29,7 @@ class hörspiel {
         this.#medien = medien;
     }
 
+    //Standart Getters
     get nummer():number{
         return this.#nummer;
     }
@@ -53,11 +54,11 @@ class hörspiel {
         return this.#beschreibung;
     }
 
-    get veröffentlichungsdatum():Date{
-        return this.#veröffentlichungdatum;
+    get veröffentlichungsdatum():veröffentlichungsdatum{
+        return this.#veröffentlichungsdatum;
     }
     
-    get gesamtdauer():Number{
+    get gesamtdauer():gesamtdauer{
         return this.#gesamtdauer;
     }
 
@@ -69,15 +70,15 @@ class hörspiel {
         return this.#sprecherrollen;
     }
 
-    get links():links[]{
+    get links():links{
         return this.#links;
     }
 
-    get ids():ids[]{
+    get ids():ids{
         return this.#ids;
     }
 
-    get medien():medien[]{
+    get medien():medium[]{
         return this.#medien;
     }
 }
@@ -99,6 +100,10 @@ class autor {
     get nachname():string{
         return this.#nachname;
     }
+
+    get vollerName():string{
+        return `${this.#vorname} ${this.#nachname}`;
+    }
     
 }
 
@@ -118,16 +123,20 @@ class hörspielskriptautor{
     get nachname():string{
         return this.#nachname;
     }
+
+    get vollerName():string{
+        return `${this.#vorname} ${this.#nachname}`
+    }
 }
 
 class kapitel {
     #titel: string;
-    #start: number;
+    #start: startTime;
     #end: number;
 
     constructor(titel: string, start: number, end: number) {
         this.#titel = titel;
-        this.#start = start;
+        this.#start = new startTime(start);
         this.#end = end;
     }
 
@@ -135,7 +144,7 @@ class kapitel {
         return this.#titel;
     }
 
-    get start():number{
+    get start(){
         return this.#start;
     }
 
@@ -229,6 +238,24 @@ class links {
         return this.#deezer;
     }
 
+    get allAsArray(){
+        return [
+            this.#json,
+            this.#ffmetadata,
+            this.#cover,
+            this.#cover2,
+            this.#cover_itunes,
+            this.#diedreifragezeichen,
+            this.#appleMusic,
+            this.#spotify,
+            this.#bookbeat,
+            this.#amazonMusic,
+            this.#amazon,
+            this.#youTubeMusic,
+            this.#deezer,
+        ]
+    }
+
     constructor(json: string, ffmetadata: string, cover: string, cover2: string[], cover_itunes: string, diedreifragezeichen: string, appleMusic: string, spotify: string, bookbeat: string, amazonMusic: string, amazon: string, youTubeMusic: string, deezer: string) {
         this.#json = json;
         this.#ffmetadata = ffmetadata;
@@ -288,6 +315,19 @@ class ids {
         return this.#deezer;
     }
 
+    get allAsArray(){
+        return [
+            this.#dreimetadaten,
+            this.#appleMusic,
+            this.#spotify,
+            this.#bookbeat,
+            this.#amazonMusic,
+            this.#amazon,
+            this.#youTubeMusic,
+            this.#deezer,
+        ]
+    }
+
 
     constructor(dreimetadaten: number, appleMusic: string, spotify: string, bookbeat: string, amazonMusic: string, amazon: string, youTubeMusic: string, deezer: string) {
         this.#dreimetadaten = dreimetadaten;
@@ -301,7 +341,7 @@ class ids {
     }
 }
 
-class medien {
+class medium {
     #tracks: kapitel[];
     #ripLog: string;
 
@@ -318,3 +358,92 @@ class medien {
         return this.#ripLog;
     }
 }
+
+class veröffentlichungsdatum{
+    #veröffentlichungsDatum:Date;
+
+    constructor(datum:Date){
+        this.#veröffentlichungsDatum = datum;
+    }
+
+    get asString():string{
+        return this.#veröffentlichungsDatum.toLocaleDateString();
+    }
+
+    get asDate():Date{
+        return this.#veröffentlichungsDatum;
+    }
+
+    get asUnixTimeStamp(){
+        return this.#veröffentlichungsDatum.valueOf();
+    }
+}
+
+class gesamtdauer{
+    #gesamtdauer:number;
+
+    constructor(gesamtdauer:number){
+        this.#gesamtdauer = gesamtdauer;
+    }
+
+    get inMilliseconds():number{
+        return this.#gesamtdauer;
+    }
+    //Vieleicht Ergebnisse Runden
+    get inSeconds():number{
+        return this.#gesamtdauer / 1000;
+    }
+
+    get inMinutes():number{
+        return this.#gesamtdauer / 60000;
+    }
+
+    get inHoures():number{
+        return this.#gesamtdauer / 3600000;
+    }
+
+    get inHoursAndMinuts():string{
+        const hours = Math.floor(this.inMinutes/60);
+        const minutes = Math.round(this.inMinutes % 60);
+        return `${hours}:${minutes}`;
+    }
+}
+
+class startTime{
+    #start:number;
+
+    get inMillisecons(){
+        return this.#start;
+    }
+
+    constructor(start:number){
+        this.#start = start;
+    }
+
+    //Runden mit in die Doku aufnehmen
+    get inMinute(){
+        return Math.round(this.#start / 60000);
+    }
+
+    get inSecond(){
+        return this.#start / 1000;
+    }
+
+    
+
+    get inHourAndMinute(){
+        const houres = Math.floor(this.#start / 3600000);
+        const minutes = Math.floor(this.#start / 60000);
+        return `${houres}:${minutes}`;
+    }
+
+    get inMinuteAndSecond(){
+        const ZeitPunkAlsSekunden = this.#start / 1000;
+        const minute = Math.floor(this.#start / 60000);
+        const sekunden = Math.round(ZeitPunkAlsSekunden - (minute * 60));
+        return `${minute}:${sekunden}`;
+    }
+
+}
+
+//TODO: Maybe bestimmte Klassen nur Intern nutzbar machen?
